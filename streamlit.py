b437a81209ec070dc5b7892d5a6091d765d3d16f
Pradeep Kumar P <61228637+pradeep1412@users.noreply.github.com>
2023-08-28 09:20:27 +0000

    add session

--- streamlit.py
+++ streamlit.py
@@ -33,18 +33,19 @@ examples = [
     "World War I, also known as the First World War, was a global war originating in Europe that lasted from 28 July 1914 to 11 November 1918. Contemporaneously described as \"the war to end all wars,\" it led to the mobilisation of more than 70 million military personnel, including 60 million Europeans, making it one of the largest wars in history. Over 16 million people died, including 9.7 million Europeans, and an additional 7.9 million were wounded. Tens of millions of people died due to genocides (including the Armenian genocide), premeditated death from starvation, massacres, and disease. Aircraft played a major role in the conflict, including in strategic bombing of population centers, the development of tanks, the use of poison gas, and the first large-scale use of aircraft in combat. \r\n\r\nThe assassination of Archduke Franz Ferdinand of Austria by Gavrilo Princip on 28 June 1914 was the trigger that set off a chain of events leading to the war. Serbia's allies, Russia and France, and Austria-Hungary's ally, Germany, were drawn into the conflict. Within weeks, the major powers of Europe were at war. The Central Powers—Germany, Austria-Hungary, the Ottoman Empire, and Bulgaria—became known as the \"Central Powers,\" while the Entente Powers—Russia, France, Britain, Italy, Japan, and the United States—became known as the \"Entente Powers.\"\r\n\r\nThe war ended with the signing of the Armistice of 11 November 1918. The Treaty of Versailles, signed on 28 June 1919, formally ended the war between Germany and the Allied Powers. The war had a profound effect on the course of the 20th century. It led to the collapse of the Austro-Hungarian, Ottoman, and Russian Empires, and the creation of new countries, including Czechoslovakia, Yugoslavia, and Poland. The war also led to the formation of the League of Nations, an international organization dedicated to preventing future wars."
   ]
 ]
-messages = []
 
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 def chat_bot(text):
-    messages.append(text)
+    st.session_state.messages.append({"sender": "user", "message": text})
     response = palm.chat(
         **defaults,
         context=context,
         examples=examples,
-        messages=messages  # Pass the messages list, not just the text
+        messages= [message["message"] for message in st.session_state.messages]  # Pass the messages list, not just the text
     )
     temp = response.last
-    messages.append(temp)
+    st.session_state.messages.append({"sender": "bot", "message": temp})
     return temp
 
 def generate_audio(text):
@@ -56,10 +57,19 @@ def generate_audio(text):
 st.title("AI Chat Bot")
 
 text = st.text_input("Enter your message")
-if st.button("Search"):
-    response = chat_bot(text)
-    st.write(response)
 
-    # Add audio play button to play the generated speech
-    if response:
-        st.audio(generate_audio(response), format='audio/ogg')
+
+if st.button("Send"):
+    if text:
+        response = chat_bot(text)
+        
+        user_messages = [message for message in st.session_state.messages if message["sender"] == "user"]
+        bot_messages = [message for message in st.session_state.messages if message["sender"] == "bot"]
+        
+        for user_msg, bot_msg in zip(reversed(user_messages), reversed(bot_messages)):
+            with st.chat_message("user"):
+                st.write(user_msg['message'])
+            with st.chat_message("assistant"):
+                st.write(bot_msg['message'])
+                if response == bot_msg['message']:
+                  st.audio(generate_audio(response), format='audio/ogg')
